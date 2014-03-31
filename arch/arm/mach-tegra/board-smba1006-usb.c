2e7a93723373c444484bb3f5dc4e837d7c26b29f
Megumi <tenchimasaki@hotmail.com>
2014-03-31 15:47:25 -0600

    Power management changes for usb and gps

--- arch/arm/mach-tegra/board-smba1006-usb.c
+++ arch/arm/mach-tegra/board-smba1006-usb.c
@@ -109,9 +109,9 @@ static struct tegra_usb_platform_data tegra_ehci1_utmi_pdata = {
         .u_data.host = {
                 .vbus_gpio = -1,
                 .vbus_reg = NULL,
-                .hot_plug = false,
+                .hot_plug = true,
                 .remote_wakeup_supported = false,
-                .power_off_on_suspend = false,
+                .power_off_on_suspend = true,
         },
         .u_cfg.utmi = {
                 .hssync_start_delay = 0,
@@ -134,7 +134,7 @@ static struct tegra_usb_platform_data tegra_ehci3_utmi_pdata = {
                 .vbus_reg = NULL,
                 .hot_plug = true,
                 .remote_wakeup_supported = false,
-                .power_off_on_suspend = false,
+                .power_off_on_suspend = true,
         },
         .u_cfg.utmi = {
                 .hssync_start_delay = 0,
