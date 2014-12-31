e161991908a8bf43dbcab7e36c9ee1b7778c2845
Tenchi Masaki <tenchimasaki@hotmail.com>
2014-12-31 11:53:55 -0700

    restore process code

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -85,10 +85,8 @@ static int try_to_freeze_tasks(bool sig_only)
 
 		if (has_wake_lock(WAKE_LOCK_SUSPEND)) {
 			++todo;
-			/*
 			wakeup = 1;
 			break;
-			*/
 		}
 		if (!todo || time_after(jiffies, end_time))
 			break;
